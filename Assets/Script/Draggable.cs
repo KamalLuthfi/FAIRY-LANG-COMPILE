1d7526e9ea0aa98bb5651214f814f9f607708199
Luthfi <mkamalluthfi@gmail.com>
2025-02-06 21:31:00 +0800

    Yes

---
+++ Assets/Script/Draggable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Vector3 startPosition;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+        canvasGroup.blocksRaycasts = false; // Allows DropZone to detect this object
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+
+        if (!DropZone.correctDrop)
+        {
+            transform.position = startPosition; // Reset position if not dropped correctly
+        }
+    }
+}
